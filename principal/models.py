49ffe62a7480ee03cfc132f7ad0c1293952fa46b
Manuel <manuelgomezsuarez@gmail.com>
2018-01-09 12:39:57 +0100

    Añade Modelo Juego

--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from idlelib.idle_test.mock_idle import Editor
 
 # Create your models here.
+class Juego(models.Model):
+    titulo = models.CharField(max_length=100,null=True)
+    desarrolladora=models.CharField(max_length=100,null=True)
+    Editor=models.CharField(max_length=100,null=True)
+    fecha_lanzamiento=models.CharField(max_length=100,null=True)
+    tamano=models.CharField(max_length=50,null=True)
+    enlace_Torrent = models.CharField(max_length=100,null=True)
+    enlace_compra=models.CharField(max_length=100,null=True)
+    enlace_gameplay=models.CharField(max_length=100,null=True)
+    info_juego=models.CharField(max_length=500,null=True)
+    def __unicode__(self):
+        return unicode(self.titulo)
+    
